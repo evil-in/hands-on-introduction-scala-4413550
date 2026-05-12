28b1db3be9d22727feb24432f2ca2f673fbd7483
Preethi-Evelyn-Sadanandan <preethievelynsadanandan@gmail.com>
2026-05-12 07:09:32 +0530

    basic main entrypoint method and function

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,1 +1,9 @@
-@main def sayHello: Unit = println("Hello world")
+@main def sayHello: Unit = sayHi("preethi")
+  // println("Hello world")
+
+def sayHi(name: String): Unit = {
+  val listN = List(1,2,3,4)
+  val things: List[String | Int | Double] = List(1, "two", 3.0)
+  println(s"Hi $name")
+  println(s"List = $listN")
+}
