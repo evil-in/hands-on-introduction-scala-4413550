42e6eb86b3a32874b6adb1fdeaae42781e3f5cb7
Preethi-Evelyn-Sadanandan <preethievelynsadanandan@gmail.com>
2026-05-12 13:17:46 +0530

    version1

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,1 +1,25 @@
-@main def sayHello: Unit = println("Hello World!")
+import scala.util.Using
+import scala.io.Source
+import java.io.{File, FileWriter}
+
+@main def run: Unit =
+  val input: String = ???
+  val output: String = ???
+  etl(input, output)
+
+def etl(inputFilePath: String, outputFilePath: String): Unit =
+  val extracted = extract(inputFilePath)
+  val transformed = transform(extracted)
+  load(transformed, outputFilePath)
+end etl
+
+def extract(input: String): List[String] = Using.resource(Source.fromFile(input))(res => res.getLines.toList)
+
+def transform(data: List[String]): List[String] = data.map(line => line.toLowerCase)
+
+def load(data: List[String], output: String = "src/main/resources/output.txt"): Unit =
+  val file = new File(output)
+  val fileWriter = new FileWriter(file)
+  fileWriter.write(data.mkString("\n"))
+  fileWriter.close()
+end load
